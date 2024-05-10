f15d1784538af753cfc4f444677e341c4fc430ff
ALvaniseo <116092256+ALvaniseo@users.noreply.github.com>
2024-05-10 09:01:18 +0200

    05_NewFeaturess_Authorization✅

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Task;
+use App\Entity\User;
 use App\Form\TaskType;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
 class TaskController extends AbstractController
@@ -90,8 +92,34 @@ class TaskController extends AbstractController
     }
 
     #[Route('/tasks/{id}/delete', name: 'task_delete', methods: ['GET','POST'])]
-    public function deleteTaskAction(Task $task)
+    public function deleteTaskAction(Task $task): Response
     {
+        $taskCreatorId = $task->getUser()->getId();
+        $taskCreator = $this->entityManager->getRepository(User::class)->findOneBy(['id' => $taskCreatorId]);
+        $currentUser = $this->getUser();
+        
+        if ($this->isGranted('ROLE_ADMIN')) {
+            if (in_array('ROLE_ANONYMOUS', $taskCreator->getRoles())) {
+                // Si l'utilisateur a les permissions nécessaires, supprimer la tâche
+                $this->entityManager->remove($task);
+                $this->entityManager->flush();
+            
+                $this->addFlash('success', 'La tâche a bien été supprimée.');
+            
+                return $this->redirectToRoute('task_list');
+            } else {
+                $this->addFlash('error', 'Vous n\'avez pas les permissions nécessaires pour supprimer cette tâche.');
+                return $this->redirectToRoute('task_list');
+            }
+        } else {
+            // Si l'utilisateur n'est pas ADMIN, il ne peut supprimer que les tâches qu'il a créées lui-même
+            if ($taskCreator !== $currentUser) {
+                $this->addFlash('error', 'Vous n\'avez pas les permissions nécessaires pour supprimer cette tâche.');
+                return $this->redirectToRoute('task_list');
+            }
+        }
+        
+        // Si l'utilisateur a les permissions nécessaires, supprimer la tâche
         $this->entityManager->remove($task);
         $this->entityManager->flush();
 
